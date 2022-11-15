ad40bac67fc8be9af3f6a23fe5626a90b3073f12
nattolecats <nattolecats@gmail.com>
2022-11-15 22:58:26 +0900

    Add pahole to allowlist to passing Kernel Building.

--- config.go
+++ config.go
@@ -118,6 +118,7 @@ var Configuration = map[string]PathConfig{
 	"unzip":   Allowed,
 	"zcat":    Allowed,
 	"zip":     Allowed,
+	"pahole":     Allowed,
 	"python3.6": Allowed,
 	"python3.7": Allowed,
 	"python3.8": Allowed,
